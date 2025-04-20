982cbf578fe2af914eb267bcd8036530e72b54e5
Mohamed Mostafa Saber <MohamedMustafaSaber>
2025-04-20 15:00:28 +0200

    Adding Authentication, Apply Logout functionality

--- JS/Home.js
+++ JS/Home.js
@@ -1,82 +1,85 @@
-let movies = [];
-let tvShows = [];
-let people = [];
-
-async function fetchTrendingMovies() {
-  const response = await fetch('https://api.themoviedb.org/3/trending/movie/day?language=en-US', {
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYmQyNDE1ZDMxOWIzOTgwODdmODlkZTFiMDI3NDc1NCIsIm5iZiI6MTY2NDc4MjAwNy4xMDMwMDAyLCJzdWIiOiI2MzNhOGViNzRmMzNhZDAwODVhZDExNTUiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.Xa73zfFcIae9R7iXVPCRiDg2dJzAJQhdgYNnVe1eQw8'
-    }
-  });
-  const data = await response.json();
-  movies = data.results.map(movie => ({
-    title: movie.title,
-    img: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
-    rating: movie.vote_average
-  }));
-
-  renderCards(movies, 'movie-list');
+if (!localStorage.getItem("sessionUserName")) {
+  window.location.href = "/loginPage.html";
+}
+else{
+  let movies = [];
+  let tvShows = [];
+  let people = [];
+  async function fetchTrendingMovies() {
+    const response = await fetch('https://api.themoviedb.org/3/trending/movie/day?language=en-US', {
+      headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYmQyNDE1ZDMxOWIzOTgwODdmODlkZTFiMDI3NDc1NCIsIm5iZiI6MTY2NDc4MjAwNy4xMDMwMDAyLCJzdWIiOiI2MzNhOGViNzRmMzNhZDAwODVhZDExNTUiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.Xa73zfFcIae9R7iXVPCRiDg2dJzAJQhdgYNnVe1eQw8'
+      }
+    });
+    const data = await response.json();
+    movies = data.results.map(movie => ({
+      title: movie.title,
+      img: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+      rating: movie.vote_average
+    }));
+    renderCards(movies, 'movie-list');
+  }
+  async function fetchTrendingTvShow() {
+    const response = await fetch('https://api.themoviedb.org/3/trending/tv/day?language=en-US', {
+      headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYmQyNDE1ZDMxOWIzOTgwODdmODlkZTFiMDI3NDc1NCIsIm5iZiI6MTY2NDc4MjAwNy4xMDMwMDAyLCJzdWIiOiI2MzNhOGViNzRmMzNhZDAwODVhZDExNTUiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.Xa73zfFcIae9R7iXVPCRiDg2dJzAJQhdgYNnVe1eQw8'
+      }
+    });
+    const data = await response.json();
+    tvShows = data.results.map(tvShow => ({
+      title: tvShow.name,
+      img: `https://image.tmdb.org/t/p/w500${tvShow.poster_path}`,
+      rating: tvShow.vote_average
+    }));
+    renderCards(tvShows, 'tv-list');
+  }
+  async function fetchTrendingPeople() {
+    const response = await fetch('https://api.themoviedb.org/3/trending/person/day?language=en-US', {
+      headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYmQyNDE1ZDMxOWIzOTgwODdmODlkZTFiMDI3NDc1NCIsIm5iZiI6MTY2NDc4MjAwNy4xMDMwMDAyLCJzdWIiOiI2MzNhOGViNzRmMzNhZDAwODVhZDExNTUiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.Xa73zfFcIae9R7iXVPCRiDg2dJzAJQhdgYNnVe1eQw8'
+      }
+    });
+    const data = await response.json();
+    people = data.results.map(person => ({
+      title: person.name,
+      img: `https://image.tmdb.org/t/p/w500${person.profile_path}`,
+      rating: person.popularity
+    }));
+    renderCards(people, 'People-list');
+  }
+  function renderCards(data, containerId) {
+    var container = document.getElementById(containerId);
+    container.innerHTML = '';
+    data.forEach(item => {
+      var card = document.createElement('div');
+      card.className = 'card';
+      card.innerHTML = `
+        <img src="${item.img}" alt="${item.title}">
+        <div class="card-rating">${item.rating}</div>
+        <div class="card-title">${item.title}</div>
+      `;
+      container.appendChild(card);
+    });
+  }
+  function search(){
+    var searchWord = document.getElementById('search').value.toLowerCase();
+    var filteredMovies = movies.filter(movie => movie.title.toLowerCase().includes(searchWord));
+    var filteredTV = tvShows.filter(tvShow => tvShow.title.toLowerCase().includes(searchWord));
+    var filteredPeople = people.filter(person => person.title.toLowerCase().includes(searchWord));
+    renderCards(filteredMovies, 'movie-list');
+    renderCards(filteredTV, 'tv-list');
+    renderCards(filteredPeople, 'People-list');
+  }
+  fetchTrendingMovies();
+  fetchTrendingTvShow();
+  fetchTrendingPeople();
+  document.getElementById('search').addEventListener('input', search);
 }
 
-async function fetchTrendingTvShow() {
-  const response = await fetch('https://api.themoviedb.org/3/trending/tv/day?language=en-US', {
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYmQyNDE1ZDMxOWIzOTgwODdmODlkZTFiMDI3NDc1NCIsIm5iZiI6MTY2NDc4MjAwNy4xMDMwMDAyLCJzdWIiOiI2MzNhOGViNzRmMzNhZDAwODVhZDExNTUiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.Xa73zfFcIae9R7iXVPCRiDg2dJzAJQhdgYNnVe1eQw8'
-    }
-  });
-  const data = await response.json();
-  tvShows = data.results.map(tvShow => ({
-    title: tvShow.name,
-    img: `https://image.tmdb.org/t/p/w500${tvShow.poster_path}`,
-    rating: tvShow.vote_average
-  }));
-
-  renderCards(tvShows, 'tv-list');
+function logout() {
+  localStorage.removeItem("sessionUserName");
+  window.location.href = "/loginPage.html";
 }
-
-async function fetchTrendingPeople() {
-  const response = await fetch('https://api.themoviedb.org/3/trending/person/day?language=en-US', {
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYmQyNDE1ZDMxOWIzOTgwODdmODlkZTFiMDI3NDc1NCIsIm5iZiI6MTY2NDc4MjAwNy4xMDMwMDAyLCJzdWIiOiI2MzNhOGViNzRmMzNhZDAwODVhZDExNTUiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.Xa73zfFcIae9R7iXVPCRiDg2dJzAJQhdgYNnVe1eQw8'
-    }
-  });
-  const data = await response.json();
-  people = data.results.map(person => ({
-    title: person.name,
-    img: `https://image.tmdb.org/t/p/w500${person.profile_path}`,
-    rating: person.popularity
-  }));
-
-  renderCards(people, 'People-list');
-}
-
-function renderCards(data, containerId) {
-  var container = document.getElementById(containerId);
-  container.innerHTML = '';
-  data.forEach(item => {
-    var card = document.createElement('div');
-    card.className = 'card';
-    card.innerHTML = `
-      <img src="${item.img}" alt="${item.title}">
-      <div class="card-rating">${item.rating}</div>
-      <div class="card-title">${item.title}</div>
-    `;
-    container.appendChild(card);
-  });
-}
-function search(){
-  var searchWord = document.getElementById('search').value.toLowerCase();
-  var filteredMovies = movies.filter(movie => movie.title.toLowerCase().includes(searchWord));
-  var filteredTV = tvShows.filter(tvShow => tvShow.title.toLowerCase().includes(searchWord));
-  var filteredPeople = people.filter(person => person.title.toLowerCase().includes(searchWord));
-  renderCards(filteredMovies, 'movie-list');
-  renderCards(filteredTV, 'tv-list');
-  renderCards(filteredPeople, 'People-list');
-}
-fetchTrendingMovies();
-fetchTrendingTvShow();
-fetchTrendingPeople();
-document.getElementById('search').addEventListener('input', search);
